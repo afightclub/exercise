dd96ca70b7b1f27ef956c07464e0f16e9d2e9427
afightclub <leeminuk0525@gmail.com>
2026-09-22 18:19:41 +0900

    [Passed] 비교 연산 (51ms, 16MB)

---
+++ trail1/비교 연산/comparison-operator.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+
+c = 1 if a >= b else 0
+print(c)
+
+d = 1 if a > b else 0
+print(d)
+
+e = 1 if a <= b else 0
+print(e)
+
+f = 1 if a < b else 0
+print(f)
+
+g = 1 if a == b else 0
+print(g)
+
+h = 1 if a != b else 0
+print(h)
